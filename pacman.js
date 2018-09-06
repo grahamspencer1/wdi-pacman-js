8b58055e985e3937b5fde928260f2389a669b65c
Graham Spencer <grahamspence@gmail.com>
2018-09-06 17:27:51 -0400

    added ghosts array and ghost menu options

--- pacman.js
+++ pacman.js
@@ -37,8 +37,7 @@ var clyde = {
   edible: false
 };
 
-// replace this comment with your four ghosts setup as objects
-
+ghosts = [inky, blinky, pinky, clyde]
 
 // Draw the screen functionality
 function drawScreen() {
@@ -61,6 +60,10 @@ function displayStats() {
 function displayMenu() {
   console.log('\n\nSelect Option:\n');  // each \n creates a new line
   console.log('(d) Eat Dot');
+  console.log('(1) Eat Inky');
+  console.log('(2) Eat Blinky');
+  console.log('(3) Eat Pinky');
+  console.log('(4) Eat Clyde');
   console.log('(q) Quit');
 }
 
